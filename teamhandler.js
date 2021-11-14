dda7573b42b6e81d9ca6c361ff06fe15d275c9b4
Daniel <dhaggre@gmail.com>
2021-11-14 01:20:11 -0500

    banner

---
+++ teamhandler.js
@@ -0,0 +1,10 @@
+fetch('players.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
