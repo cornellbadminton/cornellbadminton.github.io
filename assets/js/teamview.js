3e5a04a15e886cecdf504f859f1274b05736b86c
Daniel <dhaggre@gmail.com>
2022-02-22 16:47:10 -0500

    lastname

--- assets/js/teamview.js
+++ assets/js/teamview.js
@@ -1,6 +1,6 @@
 function appendData(data) {
     var mainContainer = document.getElementById("myData");
-    for (var i = 0; i < data.length; i+=2) {
+    /*for (var i = 0; i < data.length; i+=2) {
         //handle the first even number of team entries
         //these are handled two at a time due to two entries 
         //existing within each column
@@ -24,12 +24,12 @@ function appendData(data) {
         var hometown2 = data[i+1].hometown;
         var funfact2 = data[i+1].funfact;
 
-        var div1_1 = document.createElement("div");
-        div1_1.classList.add('rosterrow');
+        var full_row = document.createElement("div");
+        full_row.classList.add('rosterrow');
 
-        var div1_2 = document.createElement("div");
-        div1_2.classList.add('rostercolumnpfp');
-        div1_1.appendChild(div1_2);
+        var column_1_img = document.createElement("div");
+        column_1_img.classList.add('rostercolumnpfp');
+        full_row.appendChild(column_1_img);
 
         var pfpelement1 = document.createElement("img");
         pfpelement1.setAttribute('src', '../../images/' + first1.toLowerCase() + '_' + last1.toLowerCase() + '.png');
@@ -37,10 +37,10 @@ function appendData(data) {
         pfpelement1.setAttribute('height', '200px');
         pfpelement1.setAttribute('width', '200px');
         pfpelement1.setAttribute('align', 'right');
-        div1_2.appendChild(pfpelement1);
+        column_1_img.appendChild(pfpelement1);
 
-        var div1_3 = document.createElement("div");
-        div1_3.classList.add('rostercolumn');
+        var column_1_info = document.createElement("div");
+        column_1_info.classList.add('rostercolumn');
 
         var displayname1 = document.createElement('H3');
         displayname1.innerHTML = name1;
@@ -51,16 +51,16 @@ function appendData(data) {
             <b>Favorite Racket:</b> ` + favracket1 + "<br>" + `
             <b>Hometown:</b> ` + hometown1 + "<br>" + `
             <b>Fun Fact:</b> ` + funfact1
-        div1_3.appendChild(displayname1);
-        div1_3.appendChild(displaymajoryear1);
-        div1_3.appendChild(displayinfo1);
-        div1_1.appendChild(div1_3);
+        column_1_info.appendChild(displayname1);
+        column_1_info.appendChild(displaymajoryear1);
+        column_1_info.appendChild(displayinfo1);
+        full_row.appendChild(column_1_info);
 
         /////////////////////////////////////////////////////////second row in column
 
-        var div1_4 = document.createElement("div");
-        div1_4.classList.add('rostercolumnpfp');
-        div1_1.appendChild(div1_4)
+        var column_2_img = document.createElement("div");
+        column_2_img.classList.add('rostercolumnpfp');
+        full_row.appendChild(column_2_img)
 
         var pfpelement2 = document.createElement("img");
         pfpelement2.setAttribute('src', '../../images/' + first2.toLowerCase() + '_' + last2.toLowerCase() + '.png');
@@ -68,10 +68,10 @@ function appendData(data) {
         pfpelement2.setAttribute('height', '200px');
         pfpelement2.setAttribute('width', '200px');
         pfpelement2.setAttribute('align', 'right');
-        div1_4.appendChild(pfpelement2);
+        column_2_img.appendChild(pfpelement2);
 
-        var div1_5 = document.createElement("div");
-        div1_5.classList.add('rostercolumn');
+        var column_2_info = document.createElement("div");
+        column_2_info.classList.add('rostercolumn');
 
         var displayname2 = document.createElement('H3');
         displayname2.innerHTML = name2;
@@ -82,14 +82,14 @@ function appendData(data) {
             <b>Favorite Racket:</b> ` + favracket2 + "<br>" + `
             <b>Hometown:</b> ` + hometown2 + "<br>" + `
             <b>Fun Fact:</b> ` + funfact2
-        div1_5.appendChild(displayname2);
-        div1_5.appendChild(displaymajoryear2);
-        div1_5.appendChild(displayinfo2);
-        div1_1.appendChild(div1_5);
+        column_2_info.appendChild(displayname2);
+        column_2_info.appendChild(displaymajoryear2);
+        column_2_info.appendChild(displayinfo2);
+        full_row.appendChild(column_2_info);
         //var div = document.createElement("div");
         //div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-        mainContainer.appendChild(div1_1);
-    };
+        mainContainer.appendChild(full_row);
+    }*/
 
     if (true) {
         //if odd # entries, last person gets their own row
@@ -104,12 +104,12 @@ function appendData(data) {
         var hometown1 = data[i].hometown;
         var funfact1 = data[i].funfact;
 
-        var div1_1 = document.createElement("div");
-        div1_1.classList.add('rosterrow');
+        var full_row = document.createElement("div");
+        full_row.classList.add('rosterrow');
 
-        var div1_2 = document.createElement("div");
-        div1_2.classList.add('rostercolumnpfp');
-        div1_1.appendChild(div1_2);
+        var column_1_img = document.createElement("div");
+        column_1_img.classList.add('rostercolumnpfp');
+        full_row.appendChild(column_1_img);
 
         var pfpelement1 = document.createElement("img");
         pfpelement1.setAttribute('src', '../../images/' + first1.toLowerCase() + '_' + last1.toLowerCase() + '.png');
@@ -117,10 +117,10 @@ function appendData(data) {
         pfpelement1.setAttribute('height', '200px');
         pfpelement1.setAttribute('width', '200px');
         pfpelement1.setAttribute('align', 'right');
-        div1_2.appendChild(pfpelement1);
+        column_1_img.appendChild(pfpelement1);
 
-        var div1_3 = document.createElement("div");
-        div1_3.classList.add('rostercolumn');
+        var column_1_info = document.createElement("div");
+        column_1_info.classList.add('rostercolumn');
 
         var displayname1 = document.createElement('H3');
         displayname1.innerHTML = name1;
@@ -131,10 +131,19 @@ function appendData(data) {
             <b>Favorite Racket:</b> ` + favracket1 + "<br>" + `
             <b>Hometown:</b> ` + hometown1 + "<br>" + `
             <b>Fun Fact:</b> ` + funfact1
-        div1_3.appendChild(displayname1);
-        div1_3.appendChild(displaymajoryear1);
-        div1_3.appendChild(displayinfo1);
-        div1_1.appendChild(div1_3);
-        mainContainer.appendChild(div1_1);
+        column_1_info.appendChild(displayname1);
+        column_1_info.appendChild(displaymajoryear1);
+        column_1_info.appendChild(displayinfo1);
+        full_row.appendChild(column_1_info);
+
+        //filler space
+        var column_2_img = document.createElement("div");
+        column_2_img.classList.add('rostercolumnpfp');
+        full_row.appendChild(column_2_img);
+        var column_2_info = document.createElement("div");
+        column_2_info.classList.add('rostercolumn');
+        full_row.appendChild(column_2_info);
+
+        mainContainer.appendChild(full_row);
     };
-};
+}
